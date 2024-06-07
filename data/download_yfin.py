33ccdf8051fe8c170c30f79e4835b6370d46b4d4
JihoonBrianJun <jhjun0530@snu.ac.kr>
2024-06-07 19:11:38 +0900

    Added Korean stock data

--- data/download_yfin.py
+++ data/download_yfin.py
@@ -1,17 +1,31 @@
+import os
 import yfinance as yf
 from argparse import ArgumentParser
 
 def main(args):
-    tickers = ["MSFT", "AAPL", "NVDA", "GOOG", "AMZN",
-               "META", "BRK-B", "LLY", "AVGO", "JPM",
-               "TSLA", "V", "WMT", "XOM", "UNH", 
-               "MA", "PG", "COST", "JNJ", "ORCL"]
+    save_dir = os.path.join(args.save_dir, args.country)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    if args.country == 'US':
+        tickers = ["MSFT", "AAPL", "NVDA", "GOOG", "AMZN",
+                   "META", "BRK-B", "LLY", "AVGO", "JPM",
+                   "TSLA", "V", "WMT", "XOM", "UNH",
+                   "MA", "PG", "COST", "JNJ", "ORCL"]
+    elif args.country == 'KR':
+        tickers = ["005930.KS", "000660.KS", "005380.KS", "000270.KS", "068270.KS",
+                   "005490.KS", "105560.KS", "035420.KS", "006400.KS", "051910.KS",
+                   "028260.KS", "055550.KS", "012330.KS", "086790.KS", "032830.KS",
+                   "066570.KS", "000810.KS", "042700.KS", "011200.KS", "034730.KS"]
+    
     for ticker in tickers:
         data = yf.download(ticker, interval="1d", start=args.start, end=args.end)
-        data.to_csv(f'data/{ticker}_{args.start}_{args.end}.csv')
+        data.to_csv(os.path.join(save_dir, f'{ticker}_{args.start}_{args.end}.csv'))
 
 if __name__ == '__main__':
     parser = ArgumentParser()
+    parser.add_argument('--save_dir', type=str, default='data')
+    parser.add_argument('--country', type=str, default='US')
     parser.add_argument('--start', type=str, default='2000-01-01')
     parser.add_argument('--end', type=str, default='2023-12-31')
     args = parser.parse_args()
--- train_rl.py
+++ train_rl.py
@@ -22,7 +22,7 @@ def main(args):
                     "initial_lr": args.lr,
                     "gamma": args.gamma}
 
-    episode_list = preprocess_episode(args.data_path, args.horizon, args.hop, args.volume_normalizer)
+    episode_list = preprocess_episode(args.data_path, args.horizon, args.hop)
     train_episode_list = episode_list[:int(len(episode_list)*args.train_ratio)]
     test_episode_list = episode_list[int(len(episode_list)*args.train_ratio):]
     
@@ -66,19 +66,19 @@ def main(args):
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--data_path', type=str, default='data/')
+    parser.add_argument('--data_path', type=str, default='data/KR')
     parser.add_argument('--save_dir', type=str, default='ckpt/rl')
     parser.add_argument('--epoch', type=int, default=10000)
     parser.add_argument('--step_per_epoch', type=int, default=10)
     parser.add_argument('--action_bins', type=int, default=41)
     parser.add_argument('--sample_size', type=int, default=1024)
-    parser.add_argument('--horizon', type=int, default=250)
-    parser.add_argument('--hop', type=int, default=50)
-    parser.add_argument('--window', type=int, default=150)
+    parser.add_argument('--horizon', type=int, default=60)
+    parser.add_argument('--hop', type=int, default=20)
+    parser.add_argument('--window', type=int, default=10)
     parser.add_argument('--fee', type=float, default=0.0003)
     parser.add_argument('--epsilon', type=float, default=0.1)
     parser.add_argument('--train_ratio', type=float, default=0.9)
-    parser.add_argument('--bs', type=int, default=16)
+    parser.add_argument('--bs', type=int, default=128)
     parser.add_argument('--gpu', type=bool, default=False)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--weight_decay', type=float, default=0.1)
